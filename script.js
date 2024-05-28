19972ccea4fece1c55d4967d626e0f83d20f3c12
Zimo24 <123822567+Zimo24@users.noreply.github.com>
2024-05-28 09:06:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,7 @@ function countWeekdays(startDate, endDate) {
 
     while (curDate <= endDate) {
         const dayOfWeek = curDate.getDay();
-        if (dayOfWeek !== 0 && dayOfWeek !== 6) {
-            count++;
-        }
+        count++;
         curDate.setDate(curDate.getDate() + 1);
     }
     return count;
